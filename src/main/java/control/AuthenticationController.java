51e1c0c10f690cbd1fc34649cf6895bb743ceabe
Grass-Bean <ashleychanyj@gmail.com>
2025-10-22 17:16:02 +0800

    fix incorrect CR id used

--- src/main/java/control/AuthenticationController.java
+++ src/main/java/control/AuthenticationController.java
@@ -53,8 +53,7 @@ public class AuthenticationController {
     public boolean registerCompanyRepresentative(String name, String email, String password,
                                                   String companyName, String department,
                                                   String position) {
-        String userId = IdGenerator.generateUserId("CR");
-        CompanyRepresentative rep = new CompanyRepresentative(userId, password, name, email,
+        CompanyRepresentative rep = new CompanyRepresentative(email, password, name, email,
                 companyName, department, position);
         userManager.addUser(rep);
         return true;
