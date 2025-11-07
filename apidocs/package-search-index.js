42d61b61db01d493e6241005feaeffb9f1bbca6a
Audric <39489906+AudricY@users.noreply.github.com>
2025-11-07 15:29:59 +0800

    stop tracking javadocs

--- apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"boundary"},{"l":"com.internship"},{"l":"control"},{"l":"entity"},{"l":"util"}];updateSearchResults();
